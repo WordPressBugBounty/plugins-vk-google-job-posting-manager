e53238c34b890a049e3bf89ca615a133485b0caa
Sean Murphy <sean@iamseanmurphy.com>
2024-09-24 20:58:08 +0000

    1.2.16

---
+++ vk-google-job-posting-manager/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'vektor/vk-google-job-posting-manager',
+        'pretty_version' => '1.2.16',
+        'version' => '1.2.16.0',
+        'reference' => '5df31da89c27962bc637fc14c545f6fb714c4a62',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'alcohol/iso4217' => array(
+            'pretty_version' => '4.2.0',
+            'version' => '4.2.0.0',
+            'reference' => '3ce6e02fbc0501f949ffdad327f5851c04367f4b',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../alcohol/iso4217',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'vektor/vk-google-job-posting-manager' => array(
+            'pretty_version' => '1.2.16',
+            'version' => '1.2.16.0',
+            'reference' => '5df31da89c27962bc637fc14c545f6fb714c4a62',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
